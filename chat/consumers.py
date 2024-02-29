ff2d8e5037e9a3f517300ca3fa5418b12221254b
konstantine25b <102245292+konstantine25b@users.noreply.github.com>
2024-02-29 13:17:27 +0400

    added routing.py , consumers.py and made some other changes

---
+++ chat/consumers.py
@@ -0,0 +1,13 @@
+from django.contrib.auth import get_user_model
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+
+User = get_user_model()
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+        
+        
